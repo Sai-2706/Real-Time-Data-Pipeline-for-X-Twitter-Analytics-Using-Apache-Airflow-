e657ac963c4e2063af303c784b6e8c18b43e0b23
SAHITH REDDY <113358633+Sai-2706@users.noreply.github.com>
2024-12-07 19:06:11 +0530

    Update TWITTER_x.py

--- TWITTER_x.py
+++ TWITTER_x.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 def run_twitter_etl():
     # Replace with your actual credentials
-    bearer_token = "AAAAAAAAAAAAAAAAAAAAAIHYxAEAAAAAiCoPIsyyB1Cg74gX7ZI3SGMY1rI%3DLvFpEcpgSWg3w8GYr4964m5VXYbcg35tlIIEX70CVtv1FfAfaf"
+    bearer_token = "Your 'X'(twitter) bearer token"
     
     # Initialize Tweepy Client
     client = tweepy.Client(bearer_token=bearer_token)
     
     # Define the username and maximum tweets to fetch
-    username = "elonmusk"
+    username = "narendramodi"
     max_results = 50  # Adjust as needed, max is 100 per request in v2
     
     # Fetch user information to get user ID
@@ -37,8 +37,8 @@ def run_twitter_etl():
     
     # Create a DataFrame and save to CSV
     df = pd.DataFrame(tweet_list)
-    df.to_csv('refined_tweets.csv', index=False)
-    print("Tweets have been saved to 'refined_tweets.csv'.")
+    df.to_csv('Modi_tweets.csv', index=False)
+    print("Tweets have been saved to 'Modi_tweets.csv'.")
 
 # Run the ETL function
 if __name__ == "__main__":
